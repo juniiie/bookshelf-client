e91d5030a659c0f10909f9fd02e44b90a76a72a0
Climson Jr <jrbuking@gmail.com>
2022-10-19 03:05:40 +1300

    first commit

---
+++ src/pages/Add.jsx
@@ -0,0 +1,72 @@
+import axios from "axios";
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const Add = () => {
+  const [book, setBook] = useState({
+    title: "",
+    desc: "",
+    price: null,
+    cover: "",
+  });
+
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+  };
+
+  const handleClick = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("https://jun-bookshelf-app.herokuapp.com/books", book);
+      navigate("/");
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  //   console.log(book);
+
+  return (
+    <div className="form">
+      <h1>Add new Book</h1>
+      <input
+        type="text"
+        placeholder="Title"
+        name="title"
+        onChange={handleChange}
+      ></input>
+      <input
+        type="text"
+        placeholder="Author"
+        name="author"
+        onChange={handleChange}
+      ></input>
+      <input
+        type="text"
+        placeholder="Description"
+        name="desc"
+        onChange={handleChange}
+      ></input>
+      <input
+        type="number"
+        placeholder="$$$"
+        name="price"
+        onChange={handleChange}
+      ></input>
+      <input
+        type="text"
+        placeholder="Image Address Link"
+        name="cover"
+        onChange={handleChange}
+      ></input>
+      <button className="formButton button-59" onClick={handleClick}>
+        Add Book
+      </button>
+    </div>
+  );
+};
+
+export default Add;
---
+++ src/pages/Books.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+
+const Books = () => {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchAllBooks = async () => {
+      try {
+        const res = await axios.get(
+          "https://jun-bookshelf-app.herokuapp.com/books"
+        );
+        // console.log(res);
+        setBooks(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchAllBooks();
+  }, []);
+
+  const handleDelete = async (id) => {
+    try {
+      await axios.delete("https://jun-bookshelf-app.herokuapp.com/books/" + id);
+
+      window.location.reload(); // Refreshes page. Can use redux for future use
+    } catch (err) {
+      console.log("it is an errr");
+      console.log(err);
+    }
+  };
+
+  return (
+    <div>
+      <h1>My Book Shelf</h1>
+      <div className="books">
+        {books.map((book) => (
+          <div className="book" key={book.id}>
+            <div className="bookCover">
+              {book.cover && <img src={book.cover} alt="" />}
+            </div>
+
+            {/* Book Info */}
+            <div className="booksFlexContainer">
+              <h2 className="bookTitle">{book.title}</h2>
+              <h3 className="bookAuthor">{book.author}</h3>
+              <p className="bookDesc">{book.desc}</p>
+              <span className="bookPrice">${book.price}</span>
+            </div>
+            <div className="delUpBtn">
+              <button className="delete" onClick={() => handleDelete(book.id)}>
+                DELETE
+              </button>
+              <button className="update">
+                <Link to={`/update/${book.id}`} className="updateLink">
+                  UPDATE
+                </Link>
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Link to="/add">
+        <button className="button-59">Add new book</button>
+      </Link>
+    </div>
+  );
+};
+
+export default Books;
